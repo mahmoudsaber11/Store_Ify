b09d6fe8fd6a4d7a2f6e64db3af5fb88b899315c
mahmoud saber <mahmoudsaber223015@gmail.com>
2023-11-12 12:00:56 +0200

    add endpoints

--- lib/core/api/end_point.dart
+++ lib/core/api/end_point.dart
@@ -2,6 +2,9 @@ class EndPoints {
   static const String baseUrl = "https://store-ify.vercel.app/";
   static const String login = "${baseUrl}auth/login";
   static const String register = "${baseUrl}auth/signup";
+  static const String forgetPassword = "${baseUrl}auth/sendCode";
+  static const String verfiyCode = "${baseUrl}auth/CheckCode";
+  static const String resetPassword = "${baseUrl}auth/RestePassword";
 }
 
 const String token =
