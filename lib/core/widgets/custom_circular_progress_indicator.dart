0338349ced4a8a83a164bf4f8f8425b49ef9ef1c
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-11-24 03:20:49 +0200

    Update Login & Splash Features

---
+++ lib/core/widgets/custom_circular_progress_indicator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:store_ify/core/utils/app_colors.dart';
+
+class CustomCircularProgressIndicator extends StatelessWidget {
+  const CustomCircularProgressIndicator({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SpinKitFadingCircle(
+      color: AppColors.primaryColor,
+    );
+  }
+}
