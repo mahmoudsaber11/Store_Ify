d3dc6e5998162fdc401a0888aea2f4192f7038cc
mahmoud saber <mahmoudsaber223015@gmail.com>
2023-11-24 14:03:24 +0200

    add screenutil in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_ify/app.dart';
 import 'package:store_ify/bloc_observer.dart';
 import 'package:store_ify/core/utils/service_locator.dart';
@@ -10,6 +11,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await ServiceLocator().setupServiceLocator();
+  await ScreenUtil.ensureScreenSize();
 
   runApp(const StoreIfyApp());
 }
