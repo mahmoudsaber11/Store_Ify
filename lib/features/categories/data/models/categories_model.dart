3499e3f1502646dbbee95d28db0f03b3d55d0f9c
mahmoud saber <mahmoudsaber223015@gmail.com>
2023-11-29 11:40:14 +0200

    create categories model

---
+++ lib/features/categories/data/models/categories_model.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+
+class Categories extends Equatable {
+  final String id;
+  final String name;
+  final String description;
+  final CategoreyImage image;
+
+  const Categories(
+      {required this.id,
+      required this.name,
+      required this.description,
+      required this.image});
+  factory Categories.fromJson(Map<String, dynamic> json) {
+    return Categories(
+      id: json["_id"],
+      name: json["name"],
+      description: json["description"],
+      image: CategoreyImage.fromJson(json["image"]),
+    );
+  }
+  @override
+  List<Object?> get props => [id, name, description, image];
+}
+
+class CategoreyImage extends Equatable {
+  final String secureUrl;
+  final String publicId;
+
+  const CategoreyImage({required this.secureUrl, required this.publicId});
+
+  factory CategoreyImage.fromJson(Map<String, dynamic> json) {
+    return CategoreyImage(
+      secureUrl: json["secure_url"],
+      publicId: json["public_id"],
+    );
+  }
+  @override
+  List<Object?> get props => [secureUrl, publicId];
+}
